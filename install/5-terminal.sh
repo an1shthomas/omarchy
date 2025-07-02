4fabb88058ee4926f90300655eddc6c76309c93b
Anish Thomas <anishkurian.thomas@gmail.com>
2025-07-02 16:24:22 -0700

    Fix ls aliases to use lsd instead of eza
    - Replace eza package with lsd in terminal installation
    - Update ls aliases to use lsd with correct flags
    - Change lt (tree) alias to use lsd --tree --depth=2
    - Ensure consistency between installed packages and aliases

--- install/5-terminal.sh
+++ install/5-terminal.sh
@@ -1,6 +1,6 @@
 yay -S --noconfirm --needed \
   wget curl unzip inetutils zsh \
-  eza fzf ripgrep zoxide bat \
+  lsd fzf ripgrep zoxide bat \
   wl-clipboard fastfetch btop \
   man tldr less whois plocate \
   alacritty
