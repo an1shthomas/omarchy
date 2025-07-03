107e07686419bc4c3ee27c68a5f41842c573e2fa
Anish <5hsina5>
2025-07-02 18:56:01 -0700

    Fix zsh-setup.sh to use Oh My Zsh custom plugins directory
    - Replace yay package installation with direct Git cloning
    - Clone plugins to proper Oh My Zsh custom directory
    - Remove manual source commands (handled by Oh My Zsh)
    - Fixes 'plugin not found' errors

--- install/zsh-setup.sh
+++ install/zsh-setup.sh
@@ -1,18 +1,12 @@
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-# Install zsh plugins
-yay -S --noconfirm --needed zsh-autosuggestions zsh-syntax-highlighting
-
-# Create .zshrc if it doesn't exist
-touch ~/.zshrc
+# Clone zsh plugins to Oh My Zsh custom directory
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Add plugins to .zshrc
 sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
-
-# Add source commands for plugins
-echo "source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
-echo "source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
 
 # Set zsh as default shell
 chsh -s $(which zsh)
